5afbc760ca489c05f357e7d28a51266cfe8b145a
Will Johnston <will@getplatypi.com>
2015-05-15 14:29:30 -0500

    adding cli npm script

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,8 @@ import Ui from './ui/ui';
 export = function(options: { args: Array<string>; input: NodeJS.ReadableStream; output: NodeJS.WritableStream; }) {
 	var ui = new Ui(options);
 	
-	ui.info('test message');
+	ui.info('the args are:');
+	ui.info(JSON.stringify(options.args, null, 2));
 	ui.error(new Error('this is a test error'));
 	return Promise.resolve();
 }
