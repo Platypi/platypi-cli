5566fa319c45ecc4905732c3cb8c1da79c6e0ed1
Will Johnston <will@getplatypi.com>
2015-05-18 16:59:21 -0500

    help command

--- src/models/command.ts
+++ src/models/command.ts
@@ -6,6 +6,10 @@ import SilentError from '../errors/silent';
 export default class Command extends Base {
 	static name: string = 'command';
 	static aliases: Array<string> = [];
+
+	help() {
+		this.ui.info(`No help entry for command ${Command.name}.`);
+	}
 	
 	run(options: any) {
 		throw new SilentError(`The specified command ${Command.name} is invalid. For available commands see \`platypi help\``);
