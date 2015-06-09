35a0cc12532f39c3408bfe66270e676a09becd86
Will Johnston <will@getplatypi.com>
2015-06-09 14:31:48 -0500

    fixing lint issues

--- src/commands/invalid.ts
+++ src/commands/invalid.ts
@@ -3,8 +3,8 @@ import NotFoundError from '../errors/silent';
 
 export default class Invalid extends Command {
 	static commandName: string = 'invalid';
-	
-	validate(args: IParsedArgs) {
+
+	validate(args: IParsedArgs): any {
 		var command = args.commands[0];
 		throw new NotFoundError(`\`${command}\` is not a valid command.`);
 	}
