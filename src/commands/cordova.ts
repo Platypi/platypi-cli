6f115da6be009c8c805e73c2ba2dc5a4bd7b21c4
Will Johnston <will@getplatypi.com>
2015-06-26 13:29:09 -0500

    adding viewport to index

--- src/commands/cordova.ts
+++ src/commands/cordova.ts
@@ -36,8 +36,7 @@ class Cordova extends Command {
         -->
         <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *">
         <meta name="format-detection" content="telephone=no">
-        <meta name="msapplication-tap-highlight" content="no">
-        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">`;
+        <meta name="msapplication-tap-highlight" content="no">`;
 
 	private defaultComponent: IComponent = {
 		component: path.resolve(__dirname, '..', 'cordova'),
